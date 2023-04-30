cafa0649dc4a2dfcc238169a4939e4c747d655c0
user <fiveminut@mail.ru>
2023-04-30 21:07:54 +0600

    Задание 7 - 1 [SPRINT TASK 2]

--- src/main/java/kz/bitlab/crm_system/repositories/OrderRepository.java
+++
@@ -1,9 +0,0 @@
-package kz.bitlab.crm_system.repositories;
-
-import kz.bitlab.crm_system.models.OrderModel;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface OrderRepository extends JpaRepository<OrderModel, Long> {
-}
